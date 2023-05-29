11fcf640b5353013be5c5fb25191c03283666944
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-05-29 16:42:55 +0300

    Finish pokeball

--- src/poke.js
+++ src/poke.js
@@ -1,8 +1,8 @@
 /** @type {HTMLCanvasElement} **/
-let canvas = document.getElementById("pokeball");
-canvas.width = canvas.getBoundingClientRect().width;
-canvas.height = canvas.getBoundingClientRect().height;
-let ctx = canvas.getContext("2d");
+let $canvas = document.getElementById("pokeball");
+$canvas.width = $canvas.getBoundingClientRect().width;
+$canvas.height = $canvas.getBoundingClientRect().height;
+let ctx = $canvas.getContext("2d");
 ctx.translate(25, 25);
 ctx.strokeStyle = "black";
 ctx.fillStyle = "red";
@@ -24,3 +24,28 @@ ctx.stroke();
 ctx.beginPath();
 ctx.arc(100, 100, 35, 0, 2 * Math.PI, false);
 ctx.stroke();
+
+/** @type {HTMLElement | null} **/
+let $message = null;
+
+$canvas.addEventListener("click", async () => {
+    let promise = fetch("poke.json").then(r => r.json());
+    let json_array = await promise;
+    let choice = Math.round(Math.random() * json_array.length);
+    let pokemon = json_array[choice];
+    let selections = sessionStorage.getItem("selections");
+    selections = (selections === null) ? 1 : parseInt(selections) + 1;
+    sessionStorage.setItem("selections", selections);
+    if($message) $message.remove();
+    $message = document.createElement("div");
+    $message.classList.add("message");
+    $message.innerHTML =
+        `<p>${pokemon.name}, I choose you! (${selections} selections)</p>
+        <img src="${pokemon.image}" alt="${pokemon.name}">
+        <ul>
+            <li>Level: ${pokemon.level}</li>
+            <li>Abilities: ${pokemon.ability}</li>
+        </ul>
+`;
+    $canvas.insertAdjacentElement("afterend", $message);
+});
